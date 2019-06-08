2b7262dcabb47c44dd57c57bbfdbcbe718ceb9ec
jackie <jackieckc38@gmail.com>
2019-06-08 16:53:37 +0800

    Add template

---
+++ teamplate.py
@@ -0,0 +1,13 @@
+from collections import Counter, defaultdict
+from math import ceil
+from bisect import bisect_left, bisect_right
+
+
+class Solution():
+    def solve(self):
+        return 0
+
+
+for t in range(int(input())):
+    ans = Solution().solve()
+    print('Case #{}: {}'.format(t + 1, ans))
